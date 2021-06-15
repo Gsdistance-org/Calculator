28b1084bb5cae38d376c317b8825ba3837878e52
eymen <eymen@DESKTOP-VOVPP4R>
2021-06-15 22:36:46 +0300

    Proje dosyası ekle.

---
+++ Calc/Calc.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calc
+{
+    class Calc
+    {
+        public static double add(double num1, double num2)
+        {
+            return num1 + num2;
+        }
+
+        public static double subtract(double num1, double num2)
+        {
+            return num1 - num2;
+        }
+
+        public static double divide(double num1, double num2)
+        {
+            return num1 / num2;
+        }
+
+        public static double multiply(double num1, double num2)
+        {
+            return num1 * num2;
+        }
+    }
+}
---
+++ Calc/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static Calc.Calc;
+
+namespace Calc
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void richTextBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        public void error()
+        {
+            MessageBox.Show("ERROR");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double num11 = (Convert.ToDouble(this.richTextBox1.Text));
+                double num22 = (Convert.ToDouble(this.richTextBox3.Text));
+                string lus = (Convert.ToString(this.richTextBox2.Text));
+                if (lus == "+")
+                {
+                    string answer = (Convert.ToString(Calc.add(num11, num22)));
+                    MessageBox.Show(answer);
+                }
+                else if (lus == "-")
+                {
+                   string answer = (Convert.ToString(Calc.subtract(num11, num22)));
+                    MessageBox.Show(answer);
+                }
+                else if (lus == "*")
+                {
+                    string answer = (Convert.ToString(Calc.multiply(num11, num22)));
+                    MessageBox.Show(answer);
+                }
+                else if (lus == "/")
+                {
+                    string answer = (Convert.ToString(Calc.divide(num11, num22)));
+                    MessageBox.Show(answer);
+                }
+            }
+            catch(Exception ex)
+            {
+                error();
+            }
+            finally
+            {
+
+            }
+        }
+    }
+}
